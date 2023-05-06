3150f30a32e475e2d50f49b51ed0483a3c773b1a
Elvan Hidayat <elvanhd@gmail.com>
2023-05-06 14:44:17 +0700

    user login

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,5 +1,7 @@
 import {
   AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
   REGISTER_FAIL,
   REGISTER_SUCCESS,
   USER_LOADED,
@@ -24,6 +26,7 @@ export default function (state = initialState, action) {
         user: payload,
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -33,6 +36,8 @@ export default function (state = initialState, action) {
       };
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
+      console.log(123);
       localStorage.removeItem('token');
       return {
         ...state,
