ccb3fe762b23c18b0b5b8e515879a51e0b9898d4
Elvan Hidayat <elvanhd@gmail.com>
2023-05-06 15:03:54 +0700

    logout and navbar links

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -2,6 +2,7 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
+  LOGOUT,
   REGISTER_FAIL,
   REGISTER_SUCCESS,
   USER_LOADED,
@@ -37,7 +38,7 @@ export default function (state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
-      console.log(123);
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
