d846e8b187a594254c8f6abc338014b4ffd6f073
Elvan Hidayat <elvanhd@gmail.com>
2023-05-07 10:00:58 +0700

    delete education, experiences and account

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React, { Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getCurrentProfile } from '../../actions/profile';
+import { deleteAccount, getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
 import Education from './Education';
@@ -10,6 +10,7 @@ import Experience from './Experience';
 
 const Dashboard = ({
   getCurrentProfile,
+  deleteAccount,
   auth: { user },
   profile: { profile, loading },
 }) => {
@@ -30,6 +31,12 @@ const Dashboard = ({
           <DashboardActions />
           <Experience experience={profile.experience} />
           <Education education={profile.education} />
+
+          <div className='my-2'>
+            <button className='btn btn-danger' onClick={() => deleteAccount()}>
+              <i className='fas fa-user-minus' /> Delete My Account
+            </button>
+          </div>
         </Fragment>
       ) : (
         <Fragment>
@@ -45,6 +52,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
 };
@@ -54,4 +62,6 @@ const mapStateToProps = (state) => ({
   profile: state.profile,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+  Dashboard
+);
--- src/components/profile-forms/AddEducation.js
+++ src/components/profile-forms/AddEducation.js
@@ -24,20 +24,20 @@ const AddEducation = ({ addEducation, history }) => {
 
   return (
     <Fragment>
-      <h1 className='large text-primary'>Add Your Education</h1>
-      <p className='lead'>
-        <i className='fas fa-code-branch' /> Add any school or bootcamp that you
+      <h1 class='large text-primary'>Add Your Education</h1>
+      <p class='lead'>
+        <i class='fas fa-code-branch' /> Add any school or bootcamp that you
         have attended
       </p>
       <small>* = required field</small>
       <form
-        className='form'
+        class='form'
         onSubmit={(e) => {
           e.preventDefault();
           addEducation(formData, history);
         }}
       >
-        <div className='form-group'>
+        <div class='form-group'>
           <input
             type='text'
             placeholder='* School or Bootcamp'
@@ -47,7 +47,7 @@ const AddEducation = ({ addEducation, history }) => {
             required
           />
         </div>
-        <div className='form-group'>
+        <div class='form-group'>
           <input
             type='text'
             placeholder='* Degree or Certificate'
@@ -57,7 +57,7 @@ const AddEducation = ({ addEducation, history }) => {
             required
           />
         </div>
-        <div className='form-group'>
+        <div class='form-group'>
           <input
             type='text'
             placeholder='Field of Study'
@@ -66,7 +66,7 @@ const AddEducation = ({ addEducation, history }) => {
             onChange={(e) => onChange(e)}
           />
         </div>
-        <div className='form-group'>
+        <div class='form-group'>
           <h4>From Date</h4>
           <input
             type='date'
@@ -75,7 +75,7 @@ const AddEducation = ({ addEducation, history }) => {
             onChange={(e) => onChange(e)}
           />
         </div>
-        <div className='form-group'>
+        <div class='form-group'>
           <p>
             <input
               type='checkbox'
@@ -87,10 +87,10 @@ const AddEducation = ({ addEducation, history }) => {
                 toggleDisabled(!toDateDisabled);
               }}
             />{' '}
-            Current Job
+            Current School
           </p>
         </div>
-        <div className='form-group'>
+        <div class='form-group'>
           <h4>To Date</h4>
           <input
             type='date'
@@ -100,7 +100,7 @@ const AddEducation = ({ addEducation, history }) => {
             disabled={toDateDisabled ? 'disabled' : ''}
           />
         </div>
-        <div className='form-group'>
+        <div class='form-group'>
           <textarea
             name='description'
             cols='30'
@@ -110,8 +110,8 @@ const AddEducation = ({ addEducation, history }) => {
             onChange={(e) => onChange(e)}
           />
         </div>
-        <input type='submit' className='btn btn-primary my-1' />
-        <a className='btn btn-light my-1' href='dashboard'>
+        <input type='submit' class='btn btn-primary my-1' />
+        <a class='btn btn-light my-1' href='dashboard.html'>
           Go Back
         </a>
       </form>
