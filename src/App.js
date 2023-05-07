7f8fe22046fbf37bbcabbc0b04f7da1cb68d8f73
Elvan Hidayat <elvanhd@gmail.com>
2023-05-07 15:47:56 +0700

    creating posts components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import Alert from './components/layout/Alert';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Posts from './components/posts/Posts';
 import AddEducation from './components/profile-forms/AddEducation';
 import AddExperience from './components/profile-forms/AddExperience';
 import CreateProfile from './components/profile-forms/CreateProfile';
@@ -64,6 +65,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
