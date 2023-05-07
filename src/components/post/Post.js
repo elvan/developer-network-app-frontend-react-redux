82f0339280b55d0b50d1f3f6a3dfdfc377f116b9
Elvan Hidayat <elvanhd@gmail.com>
2023-05-07 17:55:08 +0700

    comments display and delete

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { getPost } from '../../actions/post';
 import Spinner from '../layout/Spinner';
 import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import PostItem from '../posts/PostItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -21,6 +22,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
